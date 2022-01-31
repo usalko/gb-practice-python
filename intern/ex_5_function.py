da84503800b9c4630fc941c475f0d36caf012527
Ivan Usalko <ivict@rambler.ru>
2022-01-31 19:23:08 +0300

    # GB-PYTHON
      -  There is the excersize 5.

--- intern/ex_5_function.py
+++ intern/ex_5_function.py
@@ -1,3 +1,16 @@
 import ast
+import re
+from math import *
 
 print('Написать программу вычисления значения функции y = f(a)')
+function = input('Введите функцию (пример: a**2 - 5): ')
+val = ast.literal_eval(input('Введите число a: '))
+
+def f(a):
+    try:
+        return eval(re.sub(r'(\W.*?|^)a(\W.*?|$)', r'\1' + f' {a} ' + r'\2', function).strip(' =\t'))
+    except NameError as e:
+        print(e)
+        exit(-1)
+
+print(f'Значение функции "{function}" для аргумента {val}: {f(val)}')
